31b823ec8d2004fa70cd3426a8d39acc6aadbed9
SebasRojasM1 <sebasrojasm1@gmail.com>
2024-07-09 15:24:16 -0500

    The errors in the code was fixed

--- src/modules/discord-bot/controllers/discord-bot.controller.ts
+++ src/modules/discord-bot/controllers/discord-bot.controller.ts
@@ -1,33 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller} from '@nestjs/common';
 import { DiscordService } from '../services/discord-bot.service';
-import { CreateDiscordTipDto, UpdateDiscordBotDto } from '../dto';
 
 @Controller('discord-bot')
 export class DiscordBotController {
   constructor(private readonly discordBotService: DiscordService) {}
 
- /*  @Post()
-  create(@Body() createDiscordBotDto: CreateDiscordBotDto) {
-    return this.discordBotService.create(createDiscordBotDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.discordBotService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.discordBotService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDiscordBotDto: UpdateDiscordBotDto) {
-    return this.discordBotService.update(+id, updateDiscordBotDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.discordBotService.remove(+id);
-  } */
 }
