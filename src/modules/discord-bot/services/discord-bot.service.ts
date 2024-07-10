f24aa0bd894b7b5fb76c5a1630862dcf8597f4d9
deiby888 <cardonadeiby88@gmail.com>
2024-07-10 08:05:41 -0500

    conflicts fixed

--- src/modules/discord-bot/services/discord-bot.service.ts
+++ src/modules/discord-bot/services/discord-bot.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Client, GatewayIntentBits} from 'discord.js';
-import { SentTipsService } from './send-tip.service';
+/* import { SentTipsService } from './send-tip.service'; */
 
 
 @Injectable()
 export class DiscordService implements OnModuleInit {
   private readonly client: Client;
 
-  constructor(private readonly sentTipsService: SentTipsService) {
+  constructor(private readonly sentTipsService: any) {
     this.client = new Client({
       intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
     });
