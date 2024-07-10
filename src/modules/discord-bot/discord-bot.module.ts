aefb11bcb157cc2be7d7e440eb92cd4604ace5c6
deiby888 <cardonadeiby88@gmail.com>
2024-07-10 07:33:19 -0500

    conflicts fixed

--- src/modules/discord-bot/discord-bot.module.ts
+++ src/modules/discord-bot/discord-bot.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DiscordService } from './services/discord-bot.service';
+/* import { DiscordService } from './services/discord-bot.service';
 import { DiscordBotController } from './controllers/discord-bot.controller';
 import { SentTipsService } from './services/send-tip.service';
-
+ */
 @Module({
-  controllers: [DiscordBotController],
-  providers: [DiscordService, SentTipsService],
+  controllers: [],
+  providers: [],
 })
 export class DiscordBotModule {}
