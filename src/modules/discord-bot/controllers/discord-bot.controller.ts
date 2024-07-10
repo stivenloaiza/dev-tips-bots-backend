1d3ea7553d110237eacd229307acef80b4ca1f41
deiby888 <cardonadeiby88@gmail.com>
2024-07-10 07:29:42 -0500

    conflicts fixed

--- src/modules/discord-bot/controllers/discord-bot.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller} from '@nestjs/common';
-import { DiscordService } from '../services/discord-bot.service';
-
-
-@Controller('discord-bot')
-export class DiscordBotController {
-  constructor(private readonly discordBotService: DiscordService) {}
-
-}
