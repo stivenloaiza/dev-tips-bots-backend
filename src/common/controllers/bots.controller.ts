876b5a65e47e7441637e6c3fa04c31cb7e433319
Richard-01 <richard.montoyaa1@gmail.com>
2024-07-16 10:55:46 -0500

    documentation with swagger

--- src/common/controllers/bots.controller.ts
+++ src/common/controllers/bots.controller.ts
@@ -7,21 +7,39 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { 
+  ApiBadRequestResponse, 
+  ApiBody, 
+  ApiInternalServerErrorResponse, 
+  ApiNotFoundResponse, 
+  ApiOkResponse, 
+  ApiOperation, 
+  ApiTags, 
+  ApiUnauthorizedResponse 
+} from '@nestjs/swagger';
 import { TipDto } from '../dtos/tipDto';
 import { ApiKeyGuard } from 'src/common/guards/api-key.guard';
 import { DiscordService } from 'src/modules/discord-bot/services/discord-bot.service';
 import { TelegramBotService } from 'src/modules/telegram-bot/services/telegram-bot.service';
 
+@ApiTags('bots')
 @Controller('bots')
 export class BotController {
   constructor(
     private readonly discordBotService: DiscordService,
     private readonly telegramBotService: TelegramBotService,
-  ) {}
+  ) { }
 
   @Post('tip')
   @UsePipes(new ValidationPipe())
   @UseGuards(ApiKeyGuard)
+  @ApiOperation({ summary: 'Send a tip to a specified channel (Discord/Telegram)' })
+  @ApiOkResponse({ description: 'Tip sent successfully!' })
+  @ApiBadRequestResponse({ description: 'Invalid input' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Channel not supported' })
+  @ApiInternalServerErrorResponse({ description: 'Server error' })
+  @ApiBody({ type: TipDto, description: 'Tip data' })
   async getTip(@Body() tipDto: TipDto) {
     if (tipDto.channel.toLowerCase() === 'discord') {
       await this.discordBotService.getTip(tipDto);
