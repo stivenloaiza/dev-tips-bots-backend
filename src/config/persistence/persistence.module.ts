a771f79952ce52670802d52da22ee2f1e9cbc99a
Julian Roman <146983577+JulianRoman1990@users.noreply.github.com>
2024-07-30 13:11:26 -0500

    changes persistence

--- src/config/persistence/persistence.module.ts
+++ src/config/persistence/persistence.module.ts
@@ -15,8 +15,10 @@ import dbConfig from './db-config';
   imports: [
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigType<typeof dbConfig>) => {
+        console.log(dbConfig())
         const { db } = configService;
-        const uriDb = `${db.mongoHost}/${db.user}:${db.password}${db.cluster}${db.name}?retryWrites=true&w=majority`;
+       
+        const uriDb = `${db.mongoHost}${db.user}:${db.password}${db.cluster}${db.name}?retryWrites=true&w=majority`;
         return {
           uri: uriDb,
         };
